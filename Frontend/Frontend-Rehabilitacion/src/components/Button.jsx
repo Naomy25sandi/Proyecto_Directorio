222fe7852b1e71152a313b1ecec2f5ece7bf35fe
Naomy25sandi <nsandi@fwdcostarica.com>
2024-09-30 19:08:58 -0600

    creacion y modificacion de componentes

--- Frontend/Frontend-Rehabilitacion/src/components/Button.jsx
+++ Frontend/Frontend-Rehabilitacion/src/components/Button.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
 // creacion del componente boton dandole props reutilizables para llamarlas y darle la funcionalidad que necesito
 
-const Button = ({ titulo, type, evento, className = "btn btn-primary" }) => {
+const Button = ({ titulo, type, evento, className = "outline-secondary" }) => {
 
     return (
         <button className={className} type={type} onClick={evento}>{titulo}</button>
