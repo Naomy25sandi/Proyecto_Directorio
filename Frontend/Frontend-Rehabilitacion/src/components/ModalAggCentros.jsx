175c495d1d41b6992eab440a6ed74f140ed13f46
Naomy25sandi <nsandi@fwdcostarica.com>
2024-10-18 11:22:02 -0600

    creacion de modal para subir informacion de los centros desde admin

---
+++ Frontend/Frontend-Rehabilitacion/src/components/ModalAggCentros.jsx
@@ -0,0 +1,48 @@
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+
+const ModalAggCentros = ({mostrar,abrir,cerrar}) => { 
+  const [nombreCentro, setNombreCentro] = useState('');
+  const [direccionCentro, setDireccionCentro] = useState('');
+  const [telefonoCentro, setTelefonoCentro] = useState('');
+  const [descripcionCentro, setDescripcionCentro] = useState('');
+  const [precioCentro, setPrecioCentro] = useState('');
+return (
+  <>
+    <Button variant="primary" onClick={abrir}>
+      Agregar centro
+    </Button>
+
+    <Modal show={mostrar} onHide={cerrar}>
+      <Modal.Header closeButton>
+        <Modal.Title>Agregar Centros</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="form-group">
+          <label>Nombre del centro</label>
+          <input type="text" className="form-control" placeholder="Nombre del centro" onChange={(e)=>setNombreCentro(e.target.value)}/>
+          <label>Dirección</label>
+          <input type="text" className="form-control" placeholder="Dirección" onChange={(e)=>setDireccionCentro(e.target.value)} />
+          <label>Telefono</label>
+          <input type="text" className="form-control" placeholder="Telefono" onChange={(e)=>setTelefonoCentro(e.target.value)}/>
+          <label>Descripción</label>
+          <input type="text" className="form-control" placeholder="Descripción" onChange={(e)=>setDescripcionCentro(e.target.value)}/>
+          <label>Precio</label>
+          <input type="text" className="form-control" placeholder="Precio"onChange={(e)=>setPrecioCentro(e.target.value)} />
+        </div>
+
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={cerrar}>
+          Cerrar la vara esta
+        </Button>
+        <Button variant="primary" onClick={cerrar}>
+          Guarde esta mica
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  </>
+);
+}     
+export default ModalAggCentros;
--- Frontend/Frontend-Rehabilitacion/src/paginas/Admin.jsx
+++ Frontend/Frontend-Rehabilitacion/src/paginas/Admin.jsx
@@ -4,10 +4,12 @@ import React, { useState, useEffect } from 'react'
 import ListaCards from '../components/ListaCards'
 import { traerCookie } from '../Services/cookies'
 import { GetData } from '../Services/api'
-
+import ModalAggCentros from '../components/ModalAggCentros'
 const Admin = () => {
   const [centros, setCentros] = useState([]);
   const esAdmin = traerCookie("super")
+  const [modal, setModal] = useState(false)
+
   useEffect(() => {
     const traerCentros = async () => {
       const centro = await GetData('centros/api/centros/')
@@ -15,13 +17,22 @@ const Admin = () => {
     }
     traerCentros()
   }, [centros])
+  
+  const abrirModal = () => {
+    setModal(true)
+  }
+  const cerrarModal = () => {
+    setModal(false)
+  } 
   return (
     <div>
       <Navbar />
 
       <ListaCards cards={centros} mostrarBotones={esAdmin} />
 
-      <Footer />
+      
+      <ModalAggCentros mostrar={modal} cerrar={cerrarModal} abrir={abrirModal}/>
+      <Footer />  
 
 
     </div>
